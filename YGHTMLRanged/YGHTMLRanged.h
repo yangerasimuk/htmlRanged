72e9d73b9f05612ee6e2cf31e01143de884b7e77
Yan Gerasimuk <mail@yangerasimuk.com>
2017-05-19 00:16:21 +0300

    Initial Commit

---
+++ YGHTMLRanged/YGHTMLRanged.h
@@ -0,0 +1,13 @@
+//
+//  YGHTMLRanged.h
+//  YGHTMLRanged
+//
+//  Created by Ян on 19/05/2017.
+//  Copyright © 2017 Yan Gerasimuk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YGHTMLRanged : NSObject
+
+@end
---
+++ YGHTMLRanged/YGHTMLRanged.m
@@ -0,0 +1,13 @@
+//
+//  YGHTMLRanged.m
+//  YGHTMLRanged
+//
+//  Created by Ян on 19/05/2017.
+//  Copyright © 2017 Yan Gerasimuk. All rights reserved.
+//
+
+#import "YGHTMLRanged.h"
+
+@implementation YGHTMLRanged
+
+@end
